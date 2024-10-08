5b6b690e6589e5bd41d2f4fb356a58715e1a5260
d088 <105545941+d088@users.noreply.github.com>
2024-10-08 20:43:59 +0300

    Delete file.py

--- file.py
+++
@@ -1,7 +0,0 @@
-def extract_zip(zip_file, extract_to):
-    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
-        zip_ref.extractall(extract_to)
-
-# Check if files are already extracted
-if not os.path.exists("car_price_prediction_model.pkl"):
-    extract_zip("car_price_prediction_model.zip", ".")
